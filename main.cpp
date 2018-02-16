835df46e4d930446fb83a658691b26555619fae8
mike <mike@astra>
2018-02-16 14:21:50 +0300

    deprecated_api. Astra Linux webkitgtk-3 usage of deprecated api enabled.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <gtk/gtk.h>
-#include <webkit2/webkit2.h>
+#include <webkit/webkit.h>
 #include <gdk/gdkkeysyms.h>
 #include <X11/Xlib.h>
 
 static void destroyWindowCb(GtkWidget* widget, GtkWidget* window);
 static gboolean closeWebViewCb(WebKitWebView* webView, GtkWidget* window);
 
-const int display_width = 1200;
-const int display_height = 600;
+const int minimize_offset = 300;
 
 bool fullscreen_state = true;
 
@@ -19,19 +18,31 @@ void swichFullscreenMode(GtkWidget *widget){
     if (fullscreen_state) {
         printf("Set sized\n");
         gtk_window_unfullscreen(GTK_WINDOW(widget));
+        gtk_window_unmaximize(GTK_WINDOW(widget));
         // set window size
-        geom.max_width = display_width;
-        geom.max_height = display_height;
+        geom.max_width = s->width - minimize_offset;
+        geom.max_height = s->height - minimize_offset;
     } else {
         printf("Set fullscreen\n");
+        gtk_window_maximize(GTK_WINDOW(widget));
         gtk_window_fullscreen(GTK_WINDOW(widget));
+
         geom.max_width = s->width;
-        geom.max_height = s->height;
+        geom.max_height = s->height;// + minimize_offset;
     }
+
+    gtk_window_set_default_size(GTK_WINDOW(widget), geom.max_width, geom.max_height);
     gtk_window_set_geometry_hints(GTK_WINDOW(widget), NULL, &geom, GDK_HINT_MAX_SIZE);
+
     fullscreen_state = !fullscreen_state;
     printf("fullscreeen state: '%s'\n", (fullscreen_state) ? "true" : "false" );
-    printf("screen size: %dx%d \n", s->width, s->height);
+    printf("screen size: %dx%d \n", geom.max_width, geom.max_height);
+}
+
+gboolean console_message_handler(WebKitWebView *web_view, gchar *message,
+        gint line, gchar *source_id, gpointer user_data)
+{
+    printf("[CONSOLE LOG]: '%s', [Line '%d'], [Source '%s'], [Pointer: %x]", message, line, source_id, web_view);
 }
 
 gboolean on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
@@ -39,7 +50,7 @@ gboolean on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_dat
   // suppress warnings
   (void) widget;
   (void) user_data;
-  // nadle reys
+  // handle keys
   switch (event->keyval)
   {
     case GDK_KEY_p:
@@ -59,7 +70,6 @@ int main(int argc, char* argv[])
 
   // Create window that will contain the browser instance
   GtkWidget* main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_default_size(GTK_WINDOW(main_window), display_width, s->height);
 
   // Define max geometry of window
 
@@ -76,12 +86,9 @@ int main(int argc, char* argv[])
   // Set up callbacks so that if either the main window or the browser instance is
   // closed, the program will exit
   g_signal_connect(main_window, "key_release_event", G_CALLBACK(on_key_release), NULL);
+  g_signal_connect(main_window, "console-message", G_CALLBACK(console_message_handler), NULL);
   g_signal_connect(main_window, "destroy", G_CALLBACK(destroyWindowCb), NULL);
   g_signal_connect(webView, "close", G_CALLBACK(closeWebViewCb), main_window);
-
-  WebKitSettings* settings = webkit_web_view_get_settings(webView);
-  webkit_settings_set_enable_write_console_messages_to_stdout(settings, TRUE);
-  webkit_web_view_set_settings(webView, settings);
 
   // Load a web page into the browser instance
   webkit_web_view_load_uri(webView, "http://localhost:8080/");
